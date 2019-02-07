8e09dd45238fa28e5cecf321026f05c89b8cc264
Eric Kanski <erickanski0402@gmail.com>
2019-02-07 11:40:29 -0600

    Initial scripts and html files

---
+++ MultilayerPerceptron.js
@@ -0,0 +1,11 @@
+let nn;
+
+function setup(){
+
+  nn = new NeuralNetwork(3,3,1);
+  //3 Input nodes, 3 Hidden nodes, 1 output node.
+}
+
+function draw(){
+
+}
---
+++ NeuralNetwork.js
@@ -0,0 +1,12 @@
+class NeuralNetwork{
+  constructor(numInput, numHidden, numOutput){
+    this.numInput = numInput;
+    this.numHidden = numHidden;
+    this.numOutput = numOutput;
+  }
+
+  feedForward(input){
+    //Matrix Math
+    //return guess
+  }
+}
