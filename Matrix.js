8d82cf51625b9faa1f11120a40e9910c75fc3712
Eric Kanski <erickanski0402@gmail.com>
2019-02-07 12:45:56 -0600

    Added print function for convenience Added static multiply function for dot product Replaced add function with map function to take generic function as an argument to apply to all values in a given matrix

--- Matrix.js
+++ Matrix.js
@@ -4,12 +4,12 @@ class Matrix{
     //Initializes a matrix of dimensions rows x cols with all zeros
     this.rows = rows;
     this.cols = cols;
-    this.matrix = [];
+    this.data = [];
 
     for(let i = 0; i < this.rows; i++){
-      this.matrix[i] = [];
+      this.data[i] = [];
       for(let j = 0; j < this.cols; j++){
-        this.matrix[i][j] = 0;
+        this.data[i][j] = 0;
       }
     }
   }
@@ -17,61 +17,54 @@ class Matrix{
   randomize(){
     for(let i = 0; i < this.rows; i++){
       for(let j = 0; j < this.cols; j++){
-        this.matrix[i][j] = Math.floor(Math.random() * 10);
+        this.data[i][j] = Math.floor(Math.random() * 10);
       }
     }
   }
 
-  //Ex. matrix.add(5);
-  add(n){
-    //Scalar and Elementwise addition of n to each value in the matrix
-    if(n instanceof Matrix){
-      for(let i = 0; i < this.rows; i++){
-        for(let j = 0; j < this.cols; j++){
-          this.matrix[i][j] += n.matrix[i][j];
-        }
-      }
-    }else{
-      for(let i = 0; i < this.rows; i++){
-        for(let j = 0; j < this.cols; j++){
-          this.matrix[i][j] += n;
-        }
+  //Ex. matrix.map(doubleIt);
+  map(func){
+    //'Lambda' function. Takes a generic function as an argument and applies
+    //  it's return value to each spot in this matrix
+    for(let i = 0; i < this.rows; i++){
+      for(let j = 0; j < this.cols; j++){
+        let val = this.data[i][j];
+        this.data[i][j] = func(val);
       }
     }
+  }
+
+  //Ex. Matrix.multiply(matrixA, matrixB);
+  //DOES NOT REQUIRE AN INSTANCE OF 'Matrix' CLASS TO USE
+  static multiply(a, b){
+    if(a.cols !== b.rows){
+      console.log("Dimensions must agree: [" + a.rows + "," + a.cols + " [" + b.rows + "," + b.cols + "]")
+      return undefined;
+    }
+
+    let result = new Matrix(a.rows, b.cols);
+
+    for(let i = 0; i < result.rows; i++){
+      for(let j = 0; j < result.cols; j++){
+        let sum = 0;
+        for(let k = 0; k < a.cols; k++){
+          sum += a.data[i][k] * b.data[k][j];
+        }
+        result.data[i][j] = sum;
+      }
+    }
+
+    return result;
   }
 
   //Ex. matrix.multiply(-3);
   multiply(n){
     //Scalar and Elementwise multiplication of n by each value in the matrix
     if(n instanceof Matrix){
-      if(this.cols !== n.rows){
-        console.log("Dimensions must agree: [" + this.rows + "," + this.cols + " [" + n.rows + "," + n.cols + "]")
-        return undefined;
-      }
-
-      let result = new Matrix(this.rows, n.cols);
-      let a = this;
-      let b = n;
-
-      for(let i = 0; i < result.rows; i++){
-        for(let j = 0; j < result.cols; j++){
-          let sum = 0;
-          for(let k = 0; k < a.cols; k++){
-            sum += a.matrix[i][k] * b.matrix[k][j];
-          }
-          result.matrix[i][j] = sum;
-        }
-      }
-
-      this.rows = result.rows;
-      this.cols = result.cols;
-      this.matrix = result.matrix;
-
-      //return result;
     }else{
       for(let i = 0; i < this.rows; i++){
         for(let j = 0; j < this.cols; j++){
-          this.matrix[i][j] *= n;
+          this.data[i][j] *= n;
         }
       }
     }
@@ -82,30 +75,14 @@ class Matrix{
 
     for(let i = 0; i < this.rows; i++){
       for(let j = 0; j < this.cols; j++){
-        result.matrix[j][i] = this.matrix[i][j];
+        result.data[j][i] = this.data[i][j];
       }
     }
 
-    this.rows = result.rows;
-    this.cols = result.cols;
-    this.matrix = result.matrix;
+    return result;
+  }
 
-    //return result;
-    // let newRows = this.cols;
-    // let newCols = this.rows;
-    // let newMatrix = [];
-    //
-    // for(let i = 0; i < this.cols; i++){
-    //   let row = [];
-    //   for(let j = 0; j < this.rows; j++){
-    //     row.push(this.matrix[i][j])
-    //   }
-    //   console.log(row)
-    //   newMatrix.push(row);
-    // }
-    //
-    // this.rows = newRows;
-    // this.cols = newCols;
-    // this.matrix = newMatrix;
+  print(){
+    console.table(this.data);
   }
 }
