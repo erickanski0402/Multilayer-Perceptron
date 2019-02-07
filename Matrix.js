2c55fc7b3d76f1575dbb7af1be6ac81852066376
Eric Kanski <erickanski0402@gmail.com>
2019-02-06 20:46:21 -0600

    Initial Matrix class to accomodate matrix math with network

---
+++ Matrix.js
@@ -0,0 +1,75 @@
+//Ex. let matrix = new Matrix(3,2);
+class Matrix{
+  constructor(rows, cols){
+    //Initializes a matrix of dimensions rows x cols with all zeros
+    this.rows = rows;
+    this.cols = cols;
+    this.matrix = [];
+
+    for(let i = 0; i < this.rows; i++){
+      this.matrix[i] = [];
+      for(let j = 0; j < this.cols; j++){
+        this.matrix[i][j] = 0;
+      }
+    }
+  }
+
+  randomize(){
+    for(let i = 0; i < this.rows; i++){
+      for(let j = 0; j < this.cols; j++){
+        this.matrix[i][j] = Math.floor(Math.random() * 10);
+      }
+    }
+  }
+
+  //Ex. matrix.add(5);
+  add(n){
+    //Scalar and Elementwise addition of n to each value in the matrix
+    if(n instanceof Matrix){
+      for(let i = 0; i < this.rows; i++){
+        for(let j = 0; j < this.cols; j++){
+          this.matrix[i][j] += n.matrix[i][j];
+        }
+      }
+    }else{
+      for(let i = 0; i < this.rows; i++){
+        for(let j = 0; j < this.cols; j++){
+          this.matrix[i][j] += n;
+        }
+      }
+    }
+  }
+
+  //Ex. matrix.multiply(-3);
+  multiply(n){
+    //Scalar and Elementwise multiplication of n by each value in the matrix
+    if(n instanceof Matrix){
+      if(this.cols !== n.rows){
+        console.log("Dimensions must agree: [" + this.rows + "," + this.cols + " [" + n.rows + "," + n.cols + "]")
+        return undefined;
+      }
+
+      let result = new Matrix(this.rows, n.cols);
+      let a = this;
+      let b = n;
+
+      for(let i = 0; i < result.rows; i++){
+        for(let j = 0; j < result.cols; j++){
+          let sum = 0;
+          for(let k = 0; k < a.cols; k++){
+            sum += a.matrix[i][k] * b.matrix[k][j];
+          }
+          result.matrix[i][j] = sum;
+        }
+      }
+
+      return result;
+    }else{
+      for(let i = 0; i < this.rows; i++){
+        for(let j = 0; j < this.cols; j++){
+          this.matrix[i][j] *= n;
+        }
+      }
+    }
+  }
+}
